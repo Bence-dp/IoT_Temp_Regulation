d7f7cd263fbf49e8241f031a663888ab3cafce55
Bence-dp <bencediplacido16@gmail.com>
2025-10-05 20:21:20 +0200

    model cpp et .h

---
+++ regul/model.h
@@ -0,0 +1,48 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <Arduino.h>
+
+// === STRUCTURE POUR LE JSON ===
+struct esp_model {
+  // --- STATUS ---
+  float temperature;
+  float luminosity;
+  bool regulationState;
+  bool fireDetected;
+  bool heaterState;
+  bool coolerState;
+  int fanSpeed;
+
+  // --- LOCATION ---
+  const char room[50] = "312";
+  const char address[200] = "Les lucioles";
+  double latitude;
+  double longitude;
+
+  // --- REGULATION ---
+  float lowThreshold;
+  float highThreshold;
+
+  // --- INFO ---
+  String ident;
+  String user;
+  String loc;
+
+  // --- NET ---
+  String uptime;
+  String WiFiSSID;
+  String MAC;
+  String IP;
+
+  // --- REPORTHOST ---
+  String target_ip;
+  int target_port;
+  int target_sp;
+};
+
+
+void initStaticInfo(esp_model &esp);
+
+
+#endif
--- regul/regul.ino
+++ regul/regul.ino
@@ -2,45 +2,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "model.h"
 
 
-// === STRUCTURE POUR LE JSON ===
-struct esp_model {
-  // --- STATUS ---
-  float temperature;
-  float luminosity;
-  bool regulationState;
-  bool fireDetected;
-  bool heaterState;
-  bool coolerState;
-  int fanSpeed;
-
-  // --- LOCATION ---
-  const char room[50] = "312";
-  const char address[200] = "Les lucioles";
-  double latitude;
-  double longitude;
-
-  // --- REGULATION ---
-  float lowThreshold;
-  float highThreshold;
-
-  // --- INFO ---
-  String ident;
-  String user;
-  String loc;
-
-  // --- NET ---
-  String uptime;
-  String WiFiSSID;
-  String MAC;
-  String IP;
-
-  // --- REPORTHOST ---
-  String target_ip;
-  int target_port;
-  int target_sp;
-};
 
 esp_model esp;
 
