46a0d4fe2df557d1ff0e9577130e50305417f73d
Daniel Carriba <dcarriba@gmx.net>
2025-10-08 16:25:47 +0200

    modif regulation_fonction.ino et ajout config.h + autres modifs

---
+++ regul/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Seuil haut et bas de la température
+#define HIGH_THRESHOLD 25
+#define LOW_THRESHOLD 24
+
+// Pin de la petite LED verte
+#define SIMPLE_LED_GREEN 19
+
+// Pin de la petite LED rouge
+#define SIMPLE_LED_RED 21
+
+// Pin de la bande de LEDs
+#define LED_PIN 13
+
+// Pin du ventilateur
+#define FANPIN 27 
+
+// Pin du capteur de lumière
+#define LUX_PIN A5
+
+// Pin du capteur de température
+#define ONE_WIRE_BUS 23
+
+#endif
